f8c5538b19a9929f2d11f55f8f7723d8652f631a
Umashankar Padhan <shankar.wp@outlook.com>
2022-05-05 11:59:28 +0000

    Updated from vs

--- src/webparts/shoppingCartWebpart/components/ShoppingCartWebpart.tsx
+++ src/webparts/shoppingCartWebpart/components/ShoppingCartWebpart.tsx
@@ -10,7 +10,7 @@ export default class ShoppingCartWebpart extends React.Component<IShoppingCartWe
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!!!!!</span>
+              <span className={ styles.title }>Welcome to SharePoint!!!!! Test</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts. test merge Welcome to merge vcode</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
